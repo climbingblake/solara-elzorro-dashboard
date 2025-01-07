d7e83fcf25d4dcd47d673f313fa072721bdf1b19
Blake <blake@blakebowling.com>
2025-01-06 19:29:20 -0500

    vedirect working BMV712

---
+++ ve_test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+
+#### call it like this
+#### python ve_test.py --port /dev/ttyUSB0
+
+
+import argparse, os
+from vedirect import VEDirect 
+
+def print_data_callback(packet):
+    print(packet)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Process VE.Direct protocol')
+    parser.add_argument('--port', help='Serial port')
+    parser.add_argument('--timeout', help='Serial port read timeout', type=int, default='60')
+    args = parser.parse_args()
+    ve = VEDirect(args.port, args.timeout)
+    print(ve.read_data_callback(print_data_callback))
